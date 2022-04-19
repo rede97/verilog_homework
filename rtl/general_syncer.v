fbbd744c0ff1e8ce09e88fa49adfc07ad39f9b8c
maxiaoqing <m454078356@163.com>
2022-04-19 19:36:33 +0800

    update signal name

---
+++ rtl/general_syncer.v
@@ -0,0 +1,17 @@
+module general_syncer (
+    input  clk,
+    input  reset_async,
+    output reset_sync
+);
+    reg [1:0] reset_dff;
+    assign reset_sync = reset_dff[1];
+
+    // reset_dff
+    always @(posedge clk or negedge reset_async) begin
+        if (!reset_async) begin
+            reset_dff <= 2'b00;
+        end else begin
+            reset_dff <= {reset_dff[0], 1'b1};
+        end
+    end
+endmodule
