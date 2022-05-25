761803d4493e2cacddbf5195d62fe713194cc5e5
maxiaoqing <m454078356@163.com>
2022-05-25 21:53:14 +0800

    complete async fifo

--- rtl/counter_wrapper.v
+++ rtl/counter_wrapper.v
@@ -1,16 +1,15 @@
 module counter_wrpper #(
     parameter CNT_WIDTH = 5
 ) (
-    input wire clk_i,  // clock input
-    input wire rstn_i, // system reset
-
-    input wire cnt_en_i,       // counter enable
-    input wire cnt_forbiden_i, // counter disable
-
-    output wire [CNT_WIDTH-1:0] addr_o,     // address output
-    output wire [  CNT_WIDTH:0] ptr_gray_o  // pointer output
+    input  wire                 clk_i,           // clock input
+    input  wire                 rstn_i,          // system reset
+    input  wire                 cnt_en_i,        // counter enable
+    input  wire                 cnt_forbiden_i,  // counter disable
+    output wire [CNT_WIDTH-1:0] addr_o,          // address output
+    output reg  [  CNT_WIDTH:0] ptr_gray_o       // pointer output
 );
-    reg [CNT_WIDTH:0] counter;  // internal counter, 1-bit more than address
+    reg  [CNT_WIDTH:0] counter;  // internal counter, 1-bit more than address
+    wire [CNT_WIDTH:0] ptr_gray;
     assign addr_o = counter[CNT_WIDTH-1:0];
 
     // update counter
@@ -24,12 +23,20 @@ module counter_wrpper #(
         end
     end
 
+    always @(posedge clk_i or negedge rstn_i) begin
+        if (!rstn_i) begin
+            ptr_gray_o <= 'h0;
+        end else begin
+            ptr_gray_o <= ptr_gray;
+        end
+    end
+
     // convert pointer to gray code
     bin2gray #(
         .DATA_WIDTH(CNT_WIDTH + 1)
     ) bin2gray_u0 (
         .data_bin_i (counter),
-        .data_gray_o(ptr_gray_o)
+        .data_gray_o(ptr_gray)
     );
 
 endmodule
